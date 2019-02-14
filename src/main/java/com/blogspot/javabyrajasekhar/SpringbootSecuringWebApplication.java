b26322f45afc2f7412c360c751116539880d698b
talk2rajasekhar <talk2rajasekhar>
2019-02-14 16:13:32 +0530

    intialcommit

---
+++ src/main/java/com/blogspot/javabyrajasekhar/SpringbootSecuringWebApplication.java
@@ -0,0 +1,14 @@
+package com.blogspot.javabyrajasekhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecuringWebApplication.class, args);
+	}
+
+}
+
